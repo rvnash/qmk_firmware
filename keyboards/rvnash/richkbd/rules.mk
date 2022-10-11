78a3d92c6f1feb8807068a5475b0871876f5c11e
Richard Nash <rvnash@gmail.com>
2022-10-11 10:31:37 -0400

    First working richkbd

--- keyboards/rvnash/richkbd/rules.mk
+++ keyboards/rvnash/richkbd/rules.mk
@@ -9,13 +9,13 @@ BOARD = GENERIC_RP_RP2040
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 
-# RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
-# WS2812_DRIVER = vendor
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+WS2812_DRIVER = vendor
 
-# OLED_ENABLE     = no
-#OLED_DRIVER     = SSD1306
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
